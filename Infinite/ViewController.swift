17f231504929f7dba82c0c17951f7fe5c149a310
Chris <christopher.harrison.2011@gmail.com>
2018-01-10 22:33:56 +0000

    Initial Commit

---
+++ Infinite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infinite
+//
+//  Created by Christopher Harrison on 10/01/2018.
+//  Copyright © 2018 Christopher Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
