62aab8fe5a7280118f4625b4a3265cc8fcd04c12
Chris <christopher.harrison.2011@gmail.com>
2018-01-11 07:24:29 +0000

    Test Change

--- Infinite/SubTaskViewController.swift
+++ Infinite/SubTaskViewController.swift
@@ -25,7 +25,7 @@ class SubTaskViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 6
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
